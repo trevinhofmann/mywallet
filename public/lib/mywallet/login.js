9010d1b184a2d85b97b88407c87759ba810ea344
Trevin Hofmann <trevinhofmann@gmail.com>
2014-10-02 21:57:37 -0500

    Create demo for login system
    This might actually be a good idea.

--- public/lib/mywallet/login.js
+++ public/lib/mywallet/login.js
@@ -6,5 +6,17 @@ function handleLoginNew(){
   alert('You clicked the new wallet button!');
 }
 
+function handleLoginGridClick(){
+  var square = this;
+  var normalColor = $(square).css('background-color');
+  $(square).css('background-color', '#0000ff');
+  setTimeout(function(){ 
+    $(square).css('background-color', normalColor);
+  }, 500);
+}
+
 $('#loginEnter').on('click', handleLoginEnter);
 $('#loginNew').on('click', handleLoginNew);
+$('.login-cell').on('click', handleLoginGridClick);
+
+$('.login-cell').css({'height':$('.login-cell').width()+'px'});
