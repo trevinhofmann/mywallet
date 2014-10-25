3d12b30541024b44df892cb662bc6ceb6f2cda93
Trevin Hofmann <trevinhofmann@gmail.com>
2014-10-25 13:09:29 -0500

    Add a prompt

--- public/lib/mywallet/index.js
+++ public/lib/mywallet/index.js
@@ -1,9 +1,22 @@
 function handleIndexNewButtonClick(){
-    alert('I should make a new wallet now.');
+  popupPrompt('Would you like a quick tutorial of MyWallet?',
+    [
+      'Yes, please!',
+      'No thanks!'
+    ],
+    [
+      function(){
+        alert('I should show you a tutorial now.');
+      },
+      function(){
+        navigateTo('wallet-home');
+      }
+    ]
+  );
 }
 
 function handleIndexLoginButtonClick(){
-    alert('I should ask how you want to log in now.');
+  alert('I should ask how you want to log in now.');
 }
 
 $('#index-new-button').on('click', handleIndexNewButtonClick);
---
+++ public/lib/mywallet/prompt.js
@@ -0,0 +1,20 @@
+function popupPrompt(prompt, choices, callbacks){
+  $('#prompt-title').text(prompt);
+  $('#prompt-options').empty();
+  for (var choice in choices){
+    $('#prompt-options').append(function(){
+      var callback = callbacks[choice];
+      return $('<br><button id="promptChoice'+escape(choice)+'" type="button" class="btn btn-primary btn-lg btn-block">'+choices[choice]+'</button>').click(function(){
+        callback();
+        closePrompt();
+      });
+    });
+  }
+  $('#prompt').show();
+}
+
+function closePrompt(){
+  $('#prompt').hide();
+}
+
+$('#prompt').hide();
