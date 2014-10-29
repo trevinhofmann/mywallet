8ae1b69f37d30b886ea4f2cb136f7047a33d57dc
Trevin Hofmann <trevinhofmann@gmail.com>
2014-10-29 15:46:59 -0500

    Add text input option for prompt

--- public/lib/mywallet/prompt.js
+++ public/lib/mywallet/prompt.js
@@ -5,11 +5,29 @@ function popupPrompt(prompt, choices, callbacks){
     $('#prompt-options').append(function(){
       var callback = callbacks[choice];
       return $('<br><button type="button" class="btn btn-primary btn-lg btn-block">'+choices[choice]+'</button>').click(function(){
-        callback();
         closePrompt();
+        callback();
       });
     });
   }
+  openPrompt();
+}
+
+function popupTextPrompt(prompt, callback){
+  $('#prompt-title').text(prompt);
+  $('#prompt-options').empty();
+  $('#prompt-options').append('<input type="text" id="popupInput">');
+  $('#prompt-options').append(function(){
+    return $('<br><button type="button" class="btn btn-primary btn-lg btn-block">Submit</button>').click(function(){
+      var value = $('#popupInput').val();
+      closePrompt();
+      callback(value);
+    });
+  });
+  openPrompt();
+}
+
+function openPrompt(){
   $('#prompt').show();
 }
 
